76f9f7a9c39f3fb09d343bbb855ff56c7fcbe8a0
AntonyNyarangi <antony.momanyi@gmail.com>
2016-10-06 20:50:17 +0300

    Update LeapYear.cpp

--- LeapYear.cpp
+++ LeapYear.cpp
@@ -1,144 +1,48 @@
 #include <iostream>
 #include <cstdlib>
 #include <pthread.h>
-#include <string>
 
 using namespace std;
 
-int accountNumber = 111222333;//identifier of the account
-int availableBalance = 500000;//amount of money in Ksh in bank
-bool accountStatus = true;//true will represent open and false, closed
-
-
-//**************************************************************//
-void Withdrawal(){
-	if (accountStatus != true){
-		cout << "Sorry, this account is closed" << endl;
-	}
-	else{
-		int withdrawalAmount;
-		cout << "How much would you like to withdraw?" << endl;
-		cin >> withdrawalAmount;
-		if (withdrawalAmount > availableBalance){
-			cout << "Insufficient Balance" << endl;
-		}
-		else{
-			availableBalance = availableBalance - withdrawalAmount;
-			cout << "Your new balance is: " << availableBalance << endl;
-		}
-	}
+void *divisibleBy4 (void *pyear)
+{
+	int year;
+	
 	pthread_exit(NULL);
 }
-//**************************************************************//
-void Deposit(){
-	if (accountStatus != true){
-		cout << "Sorry, this account is closed" << endl;
-	}
-	else{
-		int depositAmount;
-		cout << "How much would you like to deposit?" << endl;
-		cin >> depositAmount;
-		if (depositAmount < 0){
-			cout << "You cannot deposit a negative amount" << endl;
 
-		}
-		else{
-			availableBalance = availableBalance + depositAmount;
-			cout << "Your new balance is: " << availableBalance << endl;
-		}
-	}	
-	pthread_exit(NULL);
-}
-//**************************************************************//
-void CloseAccount(){
-	if (accountStatus != true){
-		cout << "Sorry, this account is already closed" << endl;
-	}
-	else{
-		accountStatus = false;
-	}
-	pthread_exit(NULL);
-}
-//**************************************************************//
-void *MobileAccess(void *){
-
-	cout << endl << "MOBILE PHONE ACCESS" << endl;
-
-	char choice;
-	cout <<"Please select an action from the list below"<<endl;
-	cout <<"1) Withdrawal"<<endl;
-	cout <<"2) Deposit"<<endl;
-	cout <<"3) Close Account"<<endl;
-	cout <<"4) Exit"<<endl;
-	cin >> choice;
-
-	switch (choice){
-		case '1': Withdrawal();
-		break;
-		case '2': Deposit();
-		break;
-		case '3': CloseAccount();
-		break;		
-		case '4': exit(1);
-		default: cout <<"Invalid Entry"<<endl;
-		MobileAccess(NULL);
-	}
-	return NULL;
-}
-//**************************************************************//
-void *InternetAccess(void *){
-
-	cout << endl << "INTERNET ACCESS" << endl;
-
-	char choice;
-	cout <<"Please select an action from the list below"<<endl;
-	cout <<"1) Withdrawal"<<endl;
-	cout <<"2) Deposit"<<endl;
-	cout <<"3) Close Account"<<endl;
-	cout <<"4) Exit"<<endl;
-	cin >> choice;
-
-	switch (choice){
-		case '1': Withdrawal();
-		break;
-		case '2': Deposit();
-		break;
-		case '3': CloseAccount();
-		break;		
-		case '4': exit(1);
-		default: cout <<"Invalid Entry"<<endl;
-		InternetAccess(NULL);
-	}
-	return NULL;
-}
-//**************************************************************//
-void *IncomingWirelessTransfer(void*){
-	if (accountStatus != true){
-		cout << "Money Transfer failed, account closed" << endl;
-	}
-	else{
-		int incomingAmount = rand() % (500000 - 20000 + 1) + 20000;
-		availableBalance = availableBalance + incomingAmount;
-	}
-	pthread_exit(NULL);
-}
-//**************************************************************//
-int main(){
-
-	// pthread_t bankThreads[2];
-	// int i;
-	// for(i = 1; i = 2; i++){
-	// 	cout << "Creating thread, "<< i
+void *divisibleBy100AND400 (void *pyear)
+{
+	int year;
+	&year = pyear;
+	//cout << year << endl;
+	// if ((year % 100 ==0) && (year % 400 == 0))
+	// {
+	// 	return 1;
 	// }
+	// else
+	// {
+	// 	return 0;
+	// }
+	pthread_exit(NULL);
+}
+
+int main()
+{
+	int year;
+	cout << "please enter the year you would like to test" << endl;
+	cin >> year;	
+
 	pthread_t thread1;
 	pthread_t thread2;
-	pthread_t thread3;
 
-	pthread_create(&thread1,NULL,IncomingWirelessTransfer,NULL);
-	pthread_create(&thread2,NULL,MobileAccess,NULL);
-	pthread_join(thread2,NULL);
-	pthread_create(&thread3,NULL,InternetAccess,NULL);
-	pthread_join(thread3,NULL);
+	pthread_create(&thread1,NULL,divisibleBy4,&year;
+	//pthread_join(thread1,NULL);
 
-	return 0;
+	pthread_create(&thread2,NULL,divisibleBy100AND400,&year);	
+	//pthread_join(thread2,NULL);
+
+
 }
+
+
